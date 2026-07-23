0094f7cb51d282d9745a8870d25c7ccdca8805b6
katymhl <katimouhli12@gmail.com>
2026-07-23 13:52:04 +0200

    modification fichier index

--- src/models/index.js
+++ src/models/index.js
@@ -1,11 +1,36 @@
+// const dbConfig = require('../config/db.config.js');
+// const { Sequelize, DataTypes } = require('sequelize');
+
+// const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+//   host: dbConfig.HOST,
+//   dialect: dbConfig.dialect,
+//   pool: dbConfig.pool
+// });
+
+// const db = {};
+// db.Sequelize = Sequelize;
+// db.sequelize = sequelize;
+
+// db.task = require('./task.model.js')(sequelize, DataTypes);
+
+// module.exports = db;
+
+
+require('dotenv').config();
+
 const dbConfig = require('../config/db.config.js');
 const { Sequelize, DataTypes } = require('sequelize');
 
-const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
-  host: dbConfig.HOST,
-  dialect: dbConfig.dialect,
-  pool: dbConfig.pool
-});
+const sequelize = new Sequelize(
+  dbConfig.DB,
+  dbConfig.USER,
+  dbConfig.PASSWORD,
+  {
+    host: dbConfig.HOST,
+    dialect: dbConfig.dialect,
+    pool: dbConfig.pool
+  }
+);
 
 const db = {};
 db.Sequelize = Sequelize;
